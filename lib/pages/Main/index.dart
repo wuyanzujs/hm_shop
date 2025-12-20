08c9aa116718dfb3fab6607842e6393ac05bf730
wuyanzu <wuyanzu@163.com>
2025-12-20 21:35:48 +0800

    Tab页面初始化

--- lib/pages/Main/index.dart
+++ lib/pages/Main/index.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hm_shop/pages/Cart/index.dart';
+import 'package:hm_shop/pages/Category/index.dart';
+import 'package:hm_shop/pages/Home/index.dart';
+import 'package:hm_shop/pages/Mine/index.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,8 +12,63 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Map<String, String>> _tabIcons = [
+    {
+      'icon': 'lib/assets/ic_public_home_normal.png',
+      'selectedIcon': 'lib/assets/ic_public_home_active.png',
+      'label': '首页',
+    },
+    {
+      'icon': 'lib/assets/ic_public_pro_normal.png',
+      'selectedIcon': 'lib/assets/ic_public_pro_active.png',
+      'label': '分类',
+    },
+    {
+      'icon': 'lib/assets/ic_public_cart_normal.png',
+      'selectedIcon': 'lib/assets/ic_public_cart_active.png',
+      'label': '购物车',
+    },
+    {
+      'icon': 'lib/assets/ic_public_my_normal.png',
+      'selectedIcon': 'lib/assets/ic_public_my_active.png',
+      'label': '我的',
+    },
+  ];
+
+  int _currentIndex = 0;
+
+  List<BottomNavigationBarItem> _getTabItems() {
+    return _tabIcons.map((tab) {
+      return BottomNavigationBarItem(
+        icon: Image.asset(tab['icon']!, width: 30, height: 30),
+        activeIcon: Image.asset(tab['selectedIcon']!, width: 30, height: 30),
+        label: tab['label']!,
+      );
+    }).toList();
+  }
+
+  List<Widget> _getTabPages() {
+    return [HomePage(), CategoryPage(), CartPage(), MinePage()];
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("Main Page")));
+    return Scaffold(
+      body: SafeArea(
+        child: IndexedStack(index: _currentIndex, children: _getTabPages()),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: _getTabItems(),
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
